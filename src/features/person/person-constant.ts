2e8da13b2187196ecb4416664066dc6b1c6f5d6e
Sergio Tran <ductran15601@gmail.com>
2022-12-27 03:42:23 +0700

    UPdate dnd code

--- src/features/person/person-constant.ts
+++ src/features/person/person-constant.ts
@@ -1,6 +1,5 @@
-export const sortableClassName = 'border ml-3 my-3';
-export const sortableItemClassName = 'border m-2 p-2 cursor-pointer';
-export const sortableChoosenItemClassName = 'bg-slate-100';
+export const sortableClassName = 'rounded bg-slate-100 shadow-sm p-4 bg-white';
+export const sortableItemClassName = 'bg-green-100 my-3 m-2 p-2 cursor-pointer bg-white rounded shadow-sm font-bold text-gray-500';
 export const sortableCommonOption = {
   animation: 150,
   group: 'person'
--- src/features/person/person-manage-ui.tsx
+++ src/features/person/person-manage-ui.tsx
@@ -1,57 +1,145 @@
-import { IPerson } from '@/models/person';
 import React from 'react';
-import { ReactSortable } from 'react-sortablejs';
-import ChildList from './components/child-list';
-import { sortableItemClassName, sortableCommonOption, sortableChoosenItemClassName, sortableClassName } from './person-constant';
+import { IPerson } from '@/models/person';
+import { closestCenter, DndContext, DragEndEvent, DragOverlay, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { sortableClassName, sortableItemClassName } from './person-constant';
+import { CSS } from '@dnd-kit/utilities';
+import { find, findIndex } from 'lodash';
+import { DragStartEvent, UniqueIdentifier } from '@dnd-kit/core/dist/types';
 
+type SortablePerson = IPerson & {
+  id: string;
+  childrens: SortablePerson[];
+}
 type Props = {
-  persons: IPerson[];
-  isChildList?: boolean;
-};
-
-const PersonManageUI: React.FC<Props> = ({ persons = [] }) => {
-  const [personList, setPersonList] = React.useState<IPerson[]>(persons);
-
-  const renderPersons = React.useMemo(
-    () =>
-      personList.map((person, index) => (
-        <div className={sortableItemClassName} key={person._id}>
-          {person.name}
-          {!!person.childrens && person.childrens.length > 0 && (
-            <ChildList
-              key={person._id}
-              id={person._id}
-              indexes={[index]}
-              list={person.childrens}
-              setList={setPersonList}
-            />
-          )}
-        </div>
-      )),
-    [personList],
+  persons: SortablePerson[]
+}
+const PersonManageUI: React.FC<Props> = ({ persons }) => {
+  const [items, setItems] = React.useState<SortablePerson[]>(
+    (persons || []).map((person) => ({
+      ...person,
+      id: person._id,
+    })),
   );
+  const [activeId, setActiveId] = React.useState<UniqueIdentifier | null>(null);
+
+  const handleDragStart = React.useCallback((e: DragStartEvent) => {
+    const { active } = e;
+    setActiveId(active.id);
+  }, []);
+
+  const handleDragEnd = React.useCallback((parentId?: string) => {
+    return (e: DragEndEvent) => {
+      if (e) {
+        const { active, over } = e;
+
+        if (!!over && active.id !== over.id) {
+          setItems((items) => {
+            const oldIndex = findIndex(items, {
+              _id: active.id as string,
+            });
+            const newIndex = findIndex(items, {
+              _id: over.id as string,
+            });
+
+            return arrayMove(items, oldIndex, newIndex);
+          });
+        }
+        setActiveId(null);
+      }
+    };
+  }, []);
 
   return (
-    <div>
-      <div className='flex mb-3'>
-        <h1 className='text-lg font-bold border-b-2 pt-2 pb-1'>
-          Person Manager
-        </h1>
-      </div>
-      <div className='flex flex-col'>
-        <ReactSortable
-          {...sortableCommonOption}
-          chosenClass={sortableChoosenItemClassName}
-          className={sortableClassName}
-          list={personList}
-          setList={setPersonList}
-        >
-          {renderPersons}
-        </ReactSortable>
-        <button>Save</button>
-      </div>
+    <SortableList
+      items={items}
+      activeId={activeId}
+      handleDragEnd={handleDragEnd}
+      handleDragStart={handleDragStart}
+    />
+  );
+};
+
+const SortableList: React.FC<{
+  items: SortablePerson[];
+  activeId: UniqueIdentifier | null,
+  handleDragStart: (e: DragStartEvent) => void;
+  handleDragEnd: (parentId?: string) => (e: DragEndEvent) => void;
+  parentId?: string;
+}> = ({ items, handleDragStart, handleDragEnd, parentId, activeId }) => {
+  const sensors = useSensors(useSensor(PointerSensor));
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd(parentId)}
+    >
+      <SortableContext
+        id={parentId}
+        items={items.map((item) => item._id)}
+        strategy={verticalListSortingStrategy}
+        key={parentId}
+      >
+        <div className={sortableClassName}>
+          {items.map((person) => {
+            return (
+              <SortableItem
+                key={person._id}
+                activeId={activeId}
+                handleDragEnd={handleDragEnd}
+                handleDragStart={handleDragStart}
+                data={person}
+              />
+            );
+          })}
+        </div>
+      </SortableContext>
+      <DragOverlay>
+        {!!activeId && <Item {...find(items, {
+          _id: activeId as string
+        }) as SortablePerson} />}
+      </DragOverlay>
+    </DndContext>
+  );
+};
+
+const SortableItem: React.FC<{
+  data: SortablePerson;
+  activeId: UniqueIdentifier | null;
+  handleDragStart: (e: DragStartEvent) => void;
+  handleDragEnd: (parentId?: string) => (e: DragEndEvent) => void;
+}> = ({ handleDragStart, handleDragEnd, data: person, activeId }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: person._id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      <Item {...person} />
+      <SortableList
+        items={person.childrens || []}
+        activeId={activeId}
+        handleDragStart={handleDragStart}
+        handleDragEnd={handleDragEnd}
+        parentId={person._id}
+      />
     </div>
   );
+};
+
+const Item: React.FC<SortablePerson> = (props) => {
+  return <div className={sortableItemClassName}>{props.name}</div>;
 };
 
 export default PersonManageUI;
